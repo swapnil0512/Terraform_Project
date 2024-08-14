3804cae153f47e13276b331af62467c2d762d2b7
swapnil0512 <104116085+swapnil0512@users.noreply.github.com>
2024-08-14 21:55:19 +0530

    Add files via upload

---
+++ Terraform_Project/backend.tf
@@ -0,0 +1,10 @@
+# terraform {
+
+#   backend "azurerm" {
+#     resource_group_name = "KubeRG"
+#     storage_account_name = "akstfswapnilkubestorage"
+#     container_name = "terraform-container"
+#     key                  = "terraform.tfstate"
+
+#   }
+# }
